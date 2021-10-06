14ac6a4ab13c15364dec02a959efd9afa5b18863
Eduardo Iturra <eduardo.iturra@karibu.cl>
2021-10-06 13:11:13 -0300

    agregar metodo

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,4 +1,4 @@
-package com.example;
+package demo.src.main.java.com.example;
 
 /**
  * Hello world!
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! "+ returnNumber() );
+    }
+
+    private static int returnNumber() {
+        return 5;
     }
 }
